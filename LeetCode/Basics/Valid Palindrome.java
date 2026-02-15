5b4d99b1df1f2cb751554e8392f30fd40123569a
Dhriti Reddy <dhritireddy2k5@gmail.com>
2026-02-15 18:50:22 +0530

    Valid Palindrome

---
+++ LeetCode/Basics/Valid Palindrome.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        s = s.replaceAll("[^a-z0-9]", "");
+        String reversed = "";
+        for(int i = s.length()-1; i>=0; i--){
+            reversed += s.charAt(i);
+        }
+        return s.equals(reversed);
+    }
+}
