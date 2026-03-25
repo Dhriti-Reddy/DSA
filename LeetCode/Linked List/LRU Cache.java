c62e32fcb13cb8eeb5a826c66f2bbb48d1bbad62
Dhriti Reddy <dhritireddy2k5@gmail.com>
2026-03-25 18:24:41 +0530

    LRU Cache

---
+++ LeetCode/Linked List/LRU Cache.java
@@ -0,0 +1,68 @@
+class LRUCache {
+    class Node {
+        int key, val;
+        Node prev, next;
+        Node(int k, int v) { key = k; val = v; }
+    }
+
+    int cap;
+    HashMap<Integer, Node> map;
+    Node head, tail;
+
+    public LRUCache(int capacity) {
+        cap = capacity;
+        map = new HashMap<>();
+
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    private void remove(Node n) {
+        n.prev.next = n.next;
+        n.next.prev = n.prev;
+    }
+
+    private void add(Node n) {
+        n.next = head.next;
+        n.prev = head;
+        head.next.prev = n;
+        head.next = n;
+    }
+    
+    public int get(int key) {
+        if (!map.containsKey(key)) return -1;
+
+        Node n = map.get(key);
+        remove(n);
+        add(n);
+        return n.val;
+    }
+    
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            Node n = map.get(key);
+            n.val = value;
+            remove(n);
+            add(n);
+        } else {
+            if (map.size() == cap) {
+                Node lru = tail.prev;
+                remove(lru);
+                map.remove(lru.key);
+            }
+            Node n = new Node(key, value);
+            map.put(key, n);
+            add(n);
+        }
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
